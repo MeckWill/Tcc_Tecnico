512f7410766671ca017b743c64c804d24dcc3f20
Meck <douglas.luiz2011@gmail.com>
2016-10-13 17:46:09 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+	header('content-type: text/html; charset=utf-8');					//
+	error_reporting (E_ALL & ~ E_NOTICE & ~ E_DEPRECATED); 				//Tira o erro de "Deprecated";
+	
+	$mysql_connect = mysql_connect('localhost:3306','root','','tcc_fj'); //Faz a conexao com o banco de dados
+	
+	if(!$mysqlTCC){ 													//verifica se esta conectando 
+		die('Nao foi possivel conectar: '.mysql_error());
+	} else {
+		echo 'Conexão sucedida!';
+	}
+	
+	mysql_close($mysql_connect); 										//fechar conexao
+?>
